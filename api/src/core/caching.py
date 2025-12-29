2ce5f15152130dc66e514b8f7aa3f893b3979939
DenViso <itdeniro00@gmail.com>
2025-12-29 21:00:58 +0200

    fix: replace redis_cli with official async Redis caching

--- api/src/core/caching.py
+++ api/src/core/caching.py
@@ -1,21 +1,27 @@
-import redis_cli
 import hashlib
 import asyncio
 import functools
 import pickle
-
 from typing import Any, Optional, Callable
+
+from redis.asyncio import Redis
 
 from .config import settings
 
 
 class RedisCaching:
+    _redis_instance: Optional[Redis] = None
+
     def __init__(self) -> None:
-        self.redis = redis_cli.get_redis()
+        if not RedisCaching._redis_instance:
+            RedisCaching._redis_instance = Redis.from_url(settings.cache.redis_url)
+        self.redis = RedisCaching._redis_instance
 
     @classmethod
     def init(cls):
-        redis_cli.init_from_url(settings.cache.redis_url)
+        """Initialize Redis client"""
+        if not cls._redis_instance:
+            cls._redis_instance = Redis.from_url(settings.cache.redis_url)
 
     @classmethod
     def get_cache_key(
@@ -26,22 +32,18 @@ class RedisCaching:
     ) -> str:
         """
         Generates a hashed cache key based on the
-        function name, prefix and namespace.
+        function name, prefix, and namespace.
         """
-        prefix = f"{prefix}:{namespace}:"
-        cache_key = (
-            prefix
-            + hashlib.md5(
-                f"{func.__module__}:{func.__name__}".encode()
-            ).hexdigest()
-        )
+        prefix_str = f"{prefix}:{namespace}:" if prefix or namespace else ""
+        key_raw = f"{func.__module__}:{func.__name__}"
+        cache_key = prefix_str + hashlib.md5(key_raw.encode()).hexdigest()
         return cache_key
 
     async def _get_processed_value(self, value: Any) -> Any:
         return pickle.loads(value) if value else None
 
     async def get(self, key: str) -> Optional[Any]:
-        value = self.redis.get(key)
+        value = await self.redis.get(key)
         return await self._get_processed_value(value)
 
     async def set(
@@ -50,15 +52,15 @@ class RedisCaching:
         value: Any,
         expire: Optional[int] = 15,
     ) -> None:
-        value = pickle.dumps(value)
+        serialized_value = pickle.dumps(value)
         if expire:
-            self.redis.setex(key, expire, value)
+            await self.redis.setex(key, expire, serialized_value)
         else:
-            self.redis.set(key, value)
+            await self.redis.set(key, serialized_value)
 
 
 def init_caching():
-    """Initialize the cache backend."""
+    """Initialize the cache backend"""
     RedisCaching.init()
 
 
@@ -79,7 +81,7 @@ def cache(
             cache_key = RedisCaching.get_cache_key(func, namespace, prefix)
 
             cached_value = await redis_caching.get(cache_key)
-            if cached_value:
+            if cached_value is not None:
                 return cached_value
 
             if asyncio.iscoroutinefunction(func):
