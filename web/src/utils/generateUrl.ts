70bc591fff8564d5167608297f0b97364d89149b
DenViso <itdeniro00@gmail.com>
2025-12-30 13:55:14 +0200

    redeploy1

--- web/src/utils/generateUrl.ts
+++ web/src/utils/generateUrl.ts
@@ -1,16 +1,23 @@
 export const generateUrl = (targetUrl: string) => {
+  // Беремо бекенд з env і змінюємо http на https
+  const BACKEND =
+    process.env.REACT_APP_BACKEND_LINK?.replace(/^http:\/\//, "https://") ??
+    "https://reliktarte-production.up.railway.app";
+
   const part = "api/v1";
 
-  const isDomainNotEndsWithSlash = !(
-  const BACKEND =
-  process.env.REACT_APP_BACKEND_LINK?.replace(/^http:\/\//, "https://") ??
-  "https://reliktarte-production.up.railway.app";
+  const isDomainNotEndsWithSlash = !BACKEND.endsWith("/");
+
+  const validPart = `${isDomainNotEndsWithSlash ? "/" : ""}${
+    !targetUrl.includes(part) ? part : ""
+  }${!targetUrl.startsWith("/") ? "/" : ""}`;
 
   const secondPart = `${validPart}${targetUrl}`.replaceAll("//", "/");
-  const url = `${
-    process.env.REACT_APP_BACKEND_LINK || "https://reliktarte-production.up.railway.app"
-  }${secondPart}`;
-console.log("🔍 ALL ENV:", process.env);
-console.log("🔍 BACKEND_LINK:", process.env.REACT_APP_BACKEND_LINK);
+
+  const url = `${BACKEND}${secondPart}`;
+
+  console.log("🔍 ALL ENV:", process.env);
+  console.log("🔍 BACKEND_LINK:", process.env.REACT_APP_BACKEND_LINK);
+
   return url;
 };
