74493b826bfbbe8364add20418022849f11c6a51
DenViso <itdeniro00@gmail.com>
2025-12-30 20:47:17 +0200

    fix https getItem.ts

--- web/src/utils/getItem.ts
+++ web/src/utils/getItem.ts
@@ -2,15 +2,29 @@ import axios from "axios";
 import { generateUrl } from "./generateUrl";
 
 export const getItems = async (url_part: string, params?: any) => {
-    let validUrl = generateUrl(url_part);
+  let validUrl = generateUrl(url_part);
 
-    if (params) {
-        const query = new URLSearchParams(params).toString();
-        if (query) {
-            validUrl += `?${query}`;
-        }
+  // Форсуємо HTTPS навіть для localhost
+  validUrl = validUrl.replace(/^http:\/\//, "https://");
+
+  // Для локального dev можна залишити http, якщо сервер не підтримує HTTPS
+  if (window.location.hostname === "localhost" && !validUrl.startsWith("https://localhost")) {
+    validUrl = validUrl.replace(/^https:\/\//, "http://");
+  }
+
+  // Додаємо query params
+  if (params) {
+    const query = new URLSearchParams(params).toString();
+    if (query) {
+      validUrl += `?${query}`;
     }
+  }
 
+  try {
     const response = await axios.get(validUrl);
     return response.data;
+  } catch (error) {
+    console.error("❌ getItems error:", error);
+    throw error;
+  }
 };
